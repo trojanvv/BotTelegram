6245c8b8cbd45a3384865e1f7e895e4fb803a3fb
v_troyan <trojanvv@gmail.com>
2017-09-14 13:15:43 +0300

    add /time

--- src/org/trojan/bot/handlerMessage.java
+++ src/org/trojan/bot/handlerMessage.java
@@ -5,6 +5,8 @@ import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 
+import java.util.Date;
+
 
 class handlerMessage {
 
@@ -17,6 +19,9 @@ class handlerMessage {
             switch (update.getMessage().getText().toLowerCase()){
                 case "/start":
                     newMessage.setText("Привет, ты попал к нам на канал");
+                    break;
+                case "/time":
+                    newMessage.setText(new Date().toString());
                     break;
                 default:
                     newMessage.setText("Такая команда отсутсвует");
